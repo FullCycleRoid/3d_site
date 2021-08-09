da6d529b546dc23069358518bf32f31f551b087d
FullCircleRoid <andrewthechf@gmail.com>
2021-08-09 17:01:09 +0300

    123

--- printing_site/static/js/scroll_slider.js
+++ printing_site/static/js/scroll_slider.js
@@ -1,30 +1,22 @@
-let process_img = document.querySelector(".process__block__img"),
-    process = document.querySelector(".process"),
-    process_scrollTop_position = process_img.scrollTop,
-    body = document.body;
-
-let processBlock =  document.querySelector(".process__block"),
-    processImg = processBlock.children[0],
-    process__line = processBlock.children[1],
-    textItemTitle = processBlock.children[2].children[0],
-    textItemParagraph = processBlock.children[2].children[1]
-
-console.log("processBlock", processBlock,
-    "processImg", processImg,
-    "process__line", process__line,
-    "textItemTitle", textItemTitle,
-    "textItemParagraph", textItemParagraph
-    )
+let body = document.body,
+    textItem = document.querySelector(".process__block__text");
 
 let scrollLock = "height: 876px; overflow: hidden;"
 let scrollUnlock = ""
 
+let processBlock =  document.querySelector(".process__block"),
+    processImg = processBlock.children[0].children[0];
+
 let imgArray = [
-    "http://localhost:8000/static/images/process/process1.jpg",
-    "http://localhost:8000/static/images/process/process2.jpg",
-    "http://localhost:8000/static/images/process/process3.jpg",
-    "http://localhost:8000/static/images/process/process4.jpg"
+    "/static/images/process/process1.jpg",
+    "/static/images/process/process2.jpg",
+    "/static/images/process/process3.jpg",
+    "/static/images/process/process4.jpg",
 ];
+
+let positions = [
+
+]
 
 let paragraphs = [
     "Детали печатаются с использованием технологии FDM / FFF из филамента состоящего на 90% из металла и 10% полимера.",
@@ -41,44 +33,93 @@ let titles = [
     "4/4 МЕТАЛЛ",
 ]
 
-// let elementsForChanging = [
-//
-// ]
 
 let counter = 0
-let parasiteCounter = 0
+setImage(counter)
 
 let onScrollHandler = function(e) {
-    let process_img_src = process_img.src
-    console.log(window.pageYOffset, "window.pageYOffset")
-
-    if (window.pageYOffset > 500 && window.pageYOffset < 1000) {
+    if (window.pageYOffset > 400 && window.pageYOffset < 1700) {
         counter = setCounter(e.deltaY, counter)
-        body.style.cssText = scrollLock;
-    }
-
-    if (counter > 4 || counter < 0) {
+        // body.style.cssText = scrollLock;
+        setImage(counter)
+        changeTitleActiveStatus(counter)
+        changeParagraphActiveStatus(counter)
+    if (counter === 4 || counter === 0) {
             body.style.cssText = scrollUnlock;
-            console.log("Unlock!")
+    }
     }
 }
 
-function changeContent(elements, counter, imgSourcesArray) {
+function changeTitleActiveStatus(counter) {
+    if (counter <= 0) {
+        textItem.children[0].style.cssText = "color: #FF4140;"
+        textItem.children[2].style.cssText = "color: #24242a;"
+        textItem.children[4].style.cssText = "color: #24242a;"
+        textItem.children[6].style.cssText = "color: #24242a;"
+    } else if (counter === 1) {
+        textItem.children[0].style.cssText = "color: #24242a;"
+        textItem.children[2].style.cssText = "color: #FF4140;"
+        textItem.children[4].style.cssText = "color: #24242a;"
+        textItem.children[6].style.cssText = "color: #24242a;"
+    } else if (counter === 2) {
+        textItem.children[0].style.cssText = "color: #24242a;"
+        textItem.children[2].style.cssText = "color: #24242a;"
+        textItem.children[4].style.cssText = "color: #FF4140;"
+        textItem.children[6].style.cssText = "color: #24242a;"
+    } else if (counter >= 3) {
+        textItem.children[0].style.cssText = "color: #24242a;"
+        textItem.children[2].style.cssText = "color: #24242a;"
+        textItem.children[4].style.cssText = "color: #24242a;"
+        textItem.children[6].style.cssText = "color: #FF4140"
+    }
+}
 
+function changeParagraphActiveStatus(counter) {
+    if (counter <= 0) {
+        textItem.children[1].style.cssText = "display: block"
+        textItem.children[3].style.cssText = "display: none"
+        textItem.children[5].style.cssText = "display: none"
+        textItem.children[7].style.cssText = "display: none"
+    } else if (counter === 1) {
+        textItem.children[1].style.cssText = "display: none"
+        textItem.children[3].style.cssText = "display: block"
+        textItem.children[5].style.cssText = "display: none"
+        textItem.children[7].style.cssText = "display: none"
+    } else if (counter === 2) {
+        textItem.children[1].style.cssText = "display: none"
+        textItem.children[3].style.cssText = "display: none"
+        textItem.children[5].style.cssText = "display: block"
+        textItem.children[7].style.cssText = "display: none"
+    } else if (counter >= 3) {
+        textItem.children[1].style.cssText = "display: none"
+        textItem.children[3].style.cssText = "display: none"
+        textItem.children[5].style.cssText = "display: none"
+        textItem.children[7].style.cssText = "display: block"
+    }
 }
 
 function setCounter(deltaY, counter) {
-    console.log("we are inside setCounter")
-    if (deltaY > 0 && counter <= 4) {
+    if (deltaY > 0 && counter < 4) {
         counter +=1
-        console.log("increase counter")
+        console.log(counter)
         return counter
-    } else if (deltaY < 0 && counter >= 0) {
+    } else if (deltaY < 0 && counter > 0) {
         counter -= 1
-        console.log("decrease Counter")
+        console.log(counter)
         return counter
     } else {
+        console.log(counter)
         return counter
+    }
+}
+
+function setImage(counter) {
+    if (counter <= 0) {
+        processImg.src = imgArray[0]
+    } else if (counter >= 3) {
+        processImg.src = imgArray[3]
+    } else {
+        processImg.src = imgArray[counter]
     }
 }
 
